7a7fe722e0fd2220af9d13c9fc1087c2747edddb
John Heger <heger7@gmail.com>
2024-02-27 18:49:55 -0600

    completed lab

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,19 @@
 class Tree:
   def __init__(self, root = None):
     self.root = root
+    
+    
 
   def get_element_by_id(self, id):
-    pass
+    return self.dfs(self.root, id)
+  
+  def dfs(self, node, target_id):
+    if node['id'] == target_id:
+      return node
+    
+    for child in node['children']:
+      result = self.dfs(child, target_id)
+      if result:
+          return result
+        
+    return None
